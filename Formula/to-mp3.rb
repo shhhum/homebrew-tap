ba8d505f6fa75d5c8bc258ee3396b8bed4d7edab
shhhum <andrewjshum@gmail.com>
2026-06-07 03:43:25 +0800

    to-mp3 0.1.2

--- Formula/to-mp3.rb
+++ Formula/to-mp3.rb
@@ -1,9 +1,9 @@
 class ToMp3 < Formula
   desc "Recursively convert audio files to high-quality 320kbps MP3"
   homepage "https://github.com/shhhum/to_mp3"
-  url "https://github.com/shhhum/to_mp3/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "c3575b9d87b1d672eff289354c52320155702dda2c99209872189420f8d030d7"
-  version "0.1.1"
+  url "https://github.com/shhhum/to_mp3/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "8d2b0aee4ff8cb8c8d024ec0f1b48f0a5fe92e5e7245a0e05a6517aba8546783"
+  version "0.1.2"
 
   depends_on "ffmpeg"
 
